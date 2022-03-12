354ce078bf64ce69d10406ace6e37c2ccaddc894
Andressa28 <andressacportela@gmail.com>
2022-03-12 15:40:41 +0000

    Início Projeto Redux

---
+++ projeto-redux/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+export default props => {
+    return (
+        <div className='Card'>
+
+            <div className='Header'>
+                <span className='Title'>{props.title}</span>
+            </div>
+
+            <div className='Content'>
+                {props.children}
+            </div>
+        </div>
+    )
+}
